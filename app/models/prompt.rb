5a58c56f47e8cb96f923f949eba35bdf6f7e7bb2
github username <github email address>
2019-11-06 11:04:15 -0500

    pass_answers to use in session serializer

--- app/models/prompt.rb
+++ app/models/prompt.rb
@@ -1,4 +1,14 @@
 class Prompt < ApplicationRecord
   has_many :answers
   belongs_to :project
+
+  def pass_answers
+    self.answers.map do |answer|
+      {
+        content: answer.content,
+        value: answer.correct
+      }
+    end
+  end
+
 end
