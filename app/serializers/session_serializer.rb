7708b983c07355e3ce7ac10a2863370811223294
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-12-09 16:47:19 -0500

    clean up

--- app/serializers/session_serializer.rb
+++ app/serializers/session_serializer.rb
@@ -9,6 +9,17 @@ class SessionSerializer < ActiveModel::Serializer
     self.object.project.user.email
   end
 
+  # def prompts
+  #   self.object.project.prompts.map do |prompt_obj|
+  #     {
+  #       id: prompt_obj.id,
+  #       content: prompt_obj.content,
+  #       image: prompt_obj.img,
+  #       answers: prompt_obj.pass_answers
+  #     }
+  #   end
+  # end
+
   def prompts
     self.object.project.prompts.map do |prompt_obj|
       {
