6161ccc48d6a4afd2d03217e8ec55ee59b0c2689
Marisa Canan-Zucker <mcananzucker@gmail.com>
2019-12-09 15:42:35 -0500

    seed data cleaned up

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,21 @@
-require 'rest-client'
-require 'pry'
-require 'JSON'
+# require 'rest-client'
+# require 'pry'
+
 
 # --- USER {email:string, first_name:string, last_name:string, avatar:string}---
 mcz = User.find_or_create_by(id: 1, email: 'marisa@email.com', first_name: 'marisa', last_name: 'canan')
 
 # --------PROJECT {title:string, subtitle:string, user_id}--------
-# proj = Project.find_or_create_by(id: 2, title: "Let's Play", subtitle: 'How well do you know your favorite board games?', user: mcz)
+game_proj = Project.find_or_create_by(id: 2, title: "Let's Play", subtitle: 'How well do you know your favorite board games?', user: mcz)
 # proj = Project.find_or_create_by(id: 1, title: 'Did you know...?', subtitle: 'random trivia', user: mcz)
-proj = Project.find_or_create_by(title: "Pass the Remote", subtitle: "TV Trivia", user: mcz)
+tv_proj = Project.find_or_create_by(title: "Pass the Remote", subtitle: "TV Trivia", user: mcz)
+
+
 # --------PROMPT {content:string, prompt_type:string, project_id} --------
 # --------ANSWER {content:string, prompt_id, correct:boolean} --------
 
-
 # <<<<<<<<<<<<<>>>>>>>>>>>>> JSON COPY/PASTED FROM API <<<<<<<<<<<<<>>>>>>>>>>>>>
-  seed_data_1 = [
+  tv_data = [
   {
       "category":"Entertainment: Television",
       "type":"multiple",
@@ -161,137 +162,134 @@ proj = Project.find_or_create_by(title: "Pass the Remote", subtitle: "TV Trivia"
   }
 ]
 
- # seed_data_2 = [
- #  {
- #      "category": "Entertainment: Board Games",
- #      "type": "multiple",
- #      "difficulty": "easy",
- #      "question": "In a standard game of Monopoly, what colour are the two cheapest properties?",
- #      "correct_answer": "Brown",
- #      "incorrect_answers": [
- #          "Green",
- #          "Yellow",
- #          "Blue"
- #      ]
- #  },
- #  {
- #      "category": "Entertainment: Board Games",
- #      "type": "multiple",
- #      "difficulty": "easy",
- #      "question": "How many dots are on a single die?",
- #      "correct_answer": "21",
- #      "incorrect_answers": [
- #          "24",
- #          "15",
- #          "18"
- #      ]
- #  },
- #  {
- #      "category": "Entertainment: Board Games",
- #      "type": "multiple",
- #      "difficulty": "medium",
- #      "question": "The board game, Nightmare was released in what year?",
- #      "correct_answer": "1991",
- #      "incorrect_answers": [
- #          "1992",
- #          "1989",
- #          "1995"
- #      ]
- #  },
- #  {
- #      "category": "Entertainment: Board Games",
- #      "type": "multiple",
- #      "difficulty": "medium",
- #      "question": "How many points is the Z tile worth in Scrabble?",
- #      "correct_answer": "10",
- #      "incorrect_answers": [
- #          "8",
- #          "5",
- #          "6"
- #      ]
- #  },
- #  {
- #      "category": "Entertainment: Board Games",
- #      "type": "multiple",
- #      "difficulty": "easy",
- #      "question": "The board game Monopoly takes its street names from which real American city?",
- #      "correct_answer": "Atlantic City, New Jersey",
- #      "incorrect_answers": [
- #          "Las Vegas, Nevada",
- #          "Duluth, Minnesota",
- #          "Charleston, South Carolina"
- #      ]
- #  },
- #  {
- #      "category": "Entertainment: Board Games",
- #      "type": "multiple",
- #      "difficulty": "hard",
- #      "question": "The board game &quot;Ra&quot; was designed by which designer? ",
- #      "correct_answer": "Reiner Knizia",
- #      "incorrect_answers": [
- #          "Bruno Cathala",
- #          "Uwe Rosenburg",
- #          "Allison Kline"
- #      ]
- #  },
- #  {
- #      "category": "Entertainment: Board Games",
- #      "type": "multiple",
- #      "difficulty": "hard",
- #      "question": "What was the development code name for the &quot;Weatherlight&quot; expansion for &quot;Magic: The Gathering&quot;, released in 1997?",
- #      "correct_answer": "Mocha Latte",
- #      "incorrect_answers": [
- #          "Decaf",
- #          "Frappuccino",
- #          "Macchiato"
- #      ]
- #  },
+ game_data = [
+  {
+      "category": "Entertainment: Board Games",
+      "type": "multiple",
+      "difficulty": "easy",
+      "question": "In a standard game of Monopoly, what colour are the two cheapest properties?",
+      "correct_answer": "Brown",
+      "incorrect_answers": [
+          "Green",
+          "Yellow",
+          "Blue"
+      ]
+  },
+  {
+      "category": "Entertainment: Board Games",
+      "type": "multiple",
+      "difficulty": "easy",
+      "question": "How many dots are on a single die?",
+      "correct_answer": "21",
+      "incorrect_answers": [
+          "24",
+          "15",
+          "18"
+      ]
+  },
+  {
+      "category": "Entertainment: Board Games",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "The board game, Nightmare was released in what year?",
+      "correct_answer": "1991",
+      "incorrect_answers": [
+          "1992",
+          "1989",
+          "1995"
+      ]
+  },
+  {
+      "category": "Entertainment: Board Games",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "How many points is the Z tile worth in Scrabble?",
+      "correct_answer": "10",
+      "incorrect_answers": [
+          "8",
+          "5",
+          "6"
+      ]
+  },
+  {
+      "category": "Entertainment: Board Games",
+      "type": "multiple",
+      "difficulty": "easy",
+      "question": "The board game Monopoly takes its street names from which real American city?",
+      "correct_answer": "Atlantic City, New Jersey",
+      "incorrect_answers": [
+          "Las Vegas, Nevada",
+          "Duluth, Minnesota",
+          "Charleston, South Carolina"
+      ]
+  },
+  {
+      "category": "Entertainment: Board Games",
+      "type": "multiple",
+      "difficulty": "hard",
+      "question": "The board game &quot;Ra&quot; was designed by which designer? ",
+      "correct_answer": "Reiner Knizia",
+      "incorrect_answers": [
+          "Bruno Cathala",
+          "Uwe Rosenburg",
+          "Allison Kline"
+      ]
+  },
+  {
+      "category": "Entertainment: Board Games",
+      "type": "multiple",
+      "difficulty": "easy",
+      "question": "When was the board game Twister, released to the public?",
+      "correct_answer": "April 1966",
+      "incorrect_answers": [
+          "September 1965",
+          "January 1969",
+          "February 1966"
+      ]
+  },
+  {
+      "category": "Entertainment: Board Games",
+      "type": "multiple",
+      "difficulty": "medium",
+      "question": "In what year was the card game Magic: the Gathering first introduced?",
+      "correct_answer": "1993",
+      "incorrect_answers": [
+          "1987",
+          "1998",
+          "2003"
+      ]
+  }]
 
- #  {
- #      "category": "Entertainment: Board Games",
- #      "type": "multiple",
- #      "difficulty": "easy",
- #      "question": "When was the board game Twister, released to the public?",
- #      "correct_answer": "April 1966",
- #      "incorrect_answers": [
- #          "September 1965",
- #          "January 1969",
- #          "February 1966"
- #      ]
- #  },
- #  {
- #      "category": "Entertainment: Board Games",
- #      "type": "multiple",
- #      "difficulty": "medium",
- #      "question": "In what year was the card game Magic: the Gathering first introduced?",
- #      "correct_answer": "1993",
- #      "incorrect_answers": [
- #          "1987",
- #          "1998",
- #          "2003"
- #      ]
- #  }]
 
 # <<<<<<<<>>>>>>>> PASS IN ARRAY OF PROMPT & 4 ANSWERS OBJECTS <<<<<<<<>>>>>>>>
-seed_data_1.each do |obj|
-  p = Prompt.create(prompt_type: "multiple-choice", project: proj, content: obj[:question])
+tv_data.each do |obj|
+  prompt = Prompt.find_or_create_by(prompt_type: "multiple-choice", project: tv_proj, content: obj[:question])
 
-    # a =
-    Answer.create(prompt: p, correct: true, content: obj[:correct_answer])
-    # a.content = obj[:correct_answer]
-    # w_one =
-    Answer.create(prompt: p, correct: false, content: obj[:incorrect_answers][0])
-    # w_one.content = obj[:incorrect_answers][0]
-    # w_two =
-    Answer.create(prompt: p, correct: false, content: obj[:incorrect_answers][1])
-    # w_two.content = obj[:incorrect_answers][1]
-    # w_three =
-    Answer.create(prompt: p, correct: false, content: obj[:incorrect_answers][2])
-    # w_three.content = obj[:incorrect_answers][2]
+
+    Answer.find_or_create_by(prompt: prompt, correct: true, content: obj[:correct_answer])
+
+    Answer.find_or_create_by(prompt: prompt, correct: false, content: obj[:incorrect_answers][0])
+
+    Answer.find_or_create_by(prompt: prompt, correct: false, content: obj[:incorrect_answers][1])
+
+    Answer.find_or_create_by(prompt: prompt, correct: false, content: obj[:incorrect_answers][2])
+
 end
 
 
+game_data.each do |obj|
+  prompt = Prompt.find_or_create_by(prompt_type: "multiple-choice", project: game_proj, content: obj[:question])
 
+
+    Answer.find_or_create_by(prompt: prompt, correct: true, content: obj[:correct_answer])
+
+    Answer.find_or_create_by(prompt: prompt, correct: false, content: obj[:incorrect_answers][0])
+
+    Answer.find_or_create_by(prompt: prompt, correct: false, content: obj[:incorrect_answers][1])
+
+    Answer.find_or_create_by(prompt: prompt, correct: false, content: obj[:incorrect_answers][2])
+
+end
 
 
 
@@ -306,8 +304,4 @@ end
 # api 10 multiple choice questions w/ answers
 # 'https://opentdb.com/api.php?amount=10&category=27&type=multiple'
 
-# fetched = RestClient::Request.execute(method: :get, url: 'https://opentdb.com/api.php?amount=10&category=27&type=multiple')
-# data = JSON.parse(fetched)
-# seed_data = data['results']
-# binding.pry
 # ***********************KEEP BETWEEN THESE COMMENTED OUT***********************
